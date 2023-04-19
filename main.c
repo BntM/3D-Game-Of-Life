3767549b5badfc4c72050c26efa52d1156d8e1e2
BntM <97204987+BntM@users.noreply.github.com>
2023-04-19 18:22:19 +0000

    idk

--- main.c
+++ main.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-  
-// main function -
-// where the execution of program begins
 int main()
 {
-  
-    // prints hello world
     printf("Hello World\n");
-  
+
     return 0;
 }
