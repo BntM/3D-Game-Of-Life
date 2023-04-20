4dd3f0b933510bbb7577b577845face8274fd81b
BntM <BinitM@protonmail.com>
2023-04-20 17:53:00 -0400

    initialize cells and started logic

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdbool.h>
+
+//chekc Cells surrounding given for num of alive cells
+int checkSurround(int midX, int midY, int midZ, bool cells[10][10][10])
+   {
+      int count = 0;
+      for(int x = midX - 1; x <= midX + 1; x++)
+      {
+         for(int y = midY - 1; y <= midY + 1; y++)
+         {
+             for(int z = midZ - 1; y <= midZ + 1; z++)
+             {
+               if(!(x == midX && y == midY && z == midZ) && cells[x][y][z]){
+                  count++;
+               }
+             }
+         }
+      }
+      return count;
+    }
+
 int main()
 {
     printf("Hello World\n");
+    //initialize cells
     bool cells[10][10][10];
     for(int x = 0; x < 10; x++)
     {
@@ -21,34 +42,23 @@ int main()
         {
             for(int z = 0; z < 10; z++)
             {
-               if(cells[x][y][z]) 
-               {
-                
-               }
-               else
-               {
-
-               }
+                int neighbours = checkSurround(x, y, z, cells);
+                if(cells[x][y][z])
+                {
+                    if(neighbours < 7 || neighbours > 20)
+                    {
+                        cells[x][y][z] = false;
+                    }
+                }
+                else
+                {
+                    if(neighbours > 7 || neighbours < 20)
+                    {
+                        cells[x][y][z] = true;
+                    }
+                }
             }
         }
     }
     return 0;
 }
-int checkSurround(int row, int col)
-   {  
-      int count = 0;
-      for(int x = row - 1; x <= row + 1; x++)
-      {
-         for(int y = col - 1; y <= col + 1; y++)
-         {
-            for(int z = )
-            if([x][y][z]){
-               if(!(r == row && c == col)){
-                  count++;
-               }
-            }
-         }
-        
-      }
-      return count;
-   }
