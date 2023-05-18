5844128a47e863a7fc7bf92f9a21f65b803ee7f5
BntM <97204987+BntM@users.noreply.github.com>
2023-05-18 18:55:37 +0000

    fixes

--- main.c
+++ main.c
@@ -1,43 +1,57 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <ncurses.h>
 
 #define xLen 4
 #define yLen 4
 #define zLen 4
 
-//https://github.com/3DSage/OpenGL-Starter_v1
+struct Matrix
+{
+
+};
 
 //Prints out the state of each cell
-int visualize(bool cells[4][4][4]) {
-    int cubeSize = 4;
+int visualize(bool cells[xLen][yLen][zLen]) {
+    initscr();
+    for(int i = 0; i < 10; i++)
+    {
+        for(int x = 0; x < i; x++)
+        {
+            printw("*");
+        }
+        refresh();
+    }
+    printw("idk");
+    /*int cubeSize = 4;
     for(int x = 0; x < cubeSize; x++) {
         printf("X");
-        printf("%d", x);
+        //printf("%d", x);
         printf("\n");
         for(int y = 0; y < cubeSize; y++) {
-            printf("Y");
-            printf("%d", y);
+            //printf("Y");
+            //printf("%d", y);
             for(int z = 0; z < cubeSize; z++) {
                 //printf(" Z");
                 //printf("%d", z);
-                printf(": ");
+                //printf(": ");
                 if(cells[x][y][z]) {
-                    printf("1");
+                    printf("*");
                 }
                 else {
-                    printf("0");
+                    printf(" ");
                 }
             }
             printf("\n");
         }
         printf("\n");
-    }
+    }*/
     return 0;
 }
 
 //chekc Cells surrounding given for num of alive cells
-int checkSurround(int midX, int midY, int midZ, bool* cells)
+int checkSurround(int midX, int midY, int midZ, bool cells[][4][4])
 {
     int count = 0;
     for(int x = midX - 1; x <= midX + 1; x++)
@@ -54,11 +68,10 @@ int checkSurround(int midX, int midY, int midZ, bool* cells)
     }
     return count;
 }
-bool* nextState(bool* cells);
+bool*** nextState(bool x[][4][4]);
 
 int main()
 {
-    printf("Hello World\n");
     //initialize cells
     bool cells[4][4][4];
     for(int x = 0; x < 4; x++)
@@ -71,30 +84,33 @@ int main()
             }
         }
     }
+    cells[2][2][2] = true;
     for(int i = 0; i < 3; i++)
     {
         visualize(cells);
-        nextState(*cells[0]);
+        bool*** output = nextState(cells);
     }
     return 0;
 }
-bool* nextState(bool* cells)
-{
+bool*** nextState(bool cells[][4][4]){
+    bool*** outputArray = malloc(sizeof(bool**) * 4);
    //change cell based on neighbours
     for(int x = 1; x < xLen - 1; x++)
     {
+        outputArray[x] = malloc(sizeof(bool**) * 4);
         for(int y = 1; y < yLen - 1; y++)
         {
+            outputArray[x][y] = malloc(sizeof(bool**) * 4);
             for(int z = 1; z < zLen - 1; z++)
             {
                 int neighbours = checkSurround(x, y, z, cells);
-                if(cells[(x * xLen) + (y * yLen) + z])
+                if(cells[x][y][z])
                 {
                   //true = false if neighbours < 9 || > 18
                     if(neighbours < 9 && neighbours > 18)
                     {
                         printf("%d %d %d\n",x , y, z);
-                        cells[(x * xLen) + (y * yLen) + z] = false;
+                        outputArray[x][y][z] = false;
                     }
                 }
                 else
@@ -103,11 +119,11 @@ bool* nextState(bool* cells)
                     if(neighbours > 9 && neighbours < 18)
                     {
                         printf("%d %d %d : %d\n",x , y, z, neighbours);
-                        cells[(x * xLen) + (y * yLen) + z] = true;
+                        outputArray[x][y][z] = true;
                     }
                 }
             }
         }
     }
-   return cells;
+   return outputArray;
 }
