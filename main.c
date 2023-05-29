cbdc5ba9ef68f845da992b89a8acfd1682248cfd
BntM <BinitM@protonmail.com>
2023-05-28 21:28:59 -0400

    update through key press

--- main.c
+++ main.c
@@ -4,14 +4,15 @@
 #include <ncurses.h>
 #include <string.h>
 
-#define xLen 4
-#define yLen 4
-#define zLen 4
+#define xLen 3
+#define yLen 10
+#define zLen 10
 
 //prints out the state of each cell
 int visualize(bool cells[xLen][yLen][zLen]) {
-    int cubesize = 4;
-    for(int x = 0; x < cubesize; x++) {
+    move(0,0);
+    int cubesize = 10;
+    for(int x = 0; x < 3; x++) {
         //printw("%d", x);
         for(int y = 0; y < cubesize; y++) {
             //printw("y");
@@ -37,7 +38,7 @@ int visualize(bool cells[xLen][yLen][zLen]) {
 }
 
 //chekc Cells surrounding given for num of alive cells
-int checkSurround(int midX, int midY, int midZ, bool cells[][4][4])
+int checkSurround(int midX, int midY, int midZ, bool cells[][10][10])
 {
     int count = 0;
     for(int x = midX - 1; x <= midX + 1; x++)
@@ -54,7 +55,7 @@ int checkSurround(int midX, int midY, int midZ, bool cells[][4][4])
     }
     return count;
 }
-bool*** nextState(bool x[][4][4]);
+bool*** nextState(bool x[][10][10]);
 
 int main()
 {
@@ -62,39 +63,48 @@ int main()
     initscr();
     cbreak();
     //initialize cells
-    bool cells[4][4][4];
-    for(int x = 0; x < 4; x++)
+    bool cells[3][10][10];
+    for(int x = 0; x < 3; x++)
     {
-        for(int y = 0; y < 4; y++)
+        for(int y = 0; y < 10; y++)
         {
-            for(int z = 0; z < 4; z++)
+            for(int z = 0; z < 10; z++)
             {
                 cells[x][y][z] = false;
             }
         }
     }
     cells[2][2][2] = true;
-    for(int i = 0; i < 1; i++)
+    //for(int i = 0; i < 3; i++)
+    //{
+    //    //visualize
+    //    visualize(cells);
+    //    memcpy(cells, nextState(cells), sizeof cells);
+    //    refresh();
+    //}
+    int c;
+    while((c=getch())!=' ')
     {
         //visualize
         visualize(cells);
         memcpy(cells, nextState(cells), sizeof cells);
+        refresh();
+
     }
     //end ncurses
-    refresh();
     getch();
     endwin();
     return 0;
 }
-bool*** nextState(bool cells[][4][4]){
-    bool*** outputArray = malloc(sizeof(bool**) * 4);
+bool*** nextState(bool cells[][10][10]){
+    bool*** outputArray = malloc(sizeof(bool**) * 10);
    //change cell based on neighbours
     for(int x = 1; x < xLen - 1; x++)
     {
-        outputArray[x] = malloc(sizeof(bool**) * 4);
+        outputArray[x] = malloc(sizeof(bool**) * 10);
         for(int y = 1; y < yLen - 1; y++)
         {
-            outputArray[x][y] = malloc(sizeof(bool**) * 4);
+            outputArray[x][y] = malloc(sizeof(bool**) * 10);
             for(int z = 1; z < zLen - 1; z++)
             {
                 int neighbours = checkSurround(x, y, z, cells);
